2df3a4ef703e5c5fe61d6a83c95a60357a48bf19
Amrit <amritdstest@gmail.com>
2023-08-16 19:29:47 +0530

    Tring

--- cypress/e2e/POMTC/EstgroNavBar.js
+++ cypress/e2e/POMTC/EstgroNavBar.js
@@ -6,14 +6,10 @@ describe("Estgro Login", () =>
   it('', () => 
   {
     const Landing = new Loginpage
-    const portal = new Portalset
-    
     Landing.enterurl();
     Landing.enterEmail();
     Landing.enterPassword();
     Landing.submit();
     Landing.logout()
   });
-
- 
   });
---
+++ cypress/e2e/TestScripts/iframe.js
@@ -0,0 +1,7 @@
+import 'cypress-iframe';
+/// <reference types ="cypress-iframe"/>
+
+describe("iFrame", () => 
+{
+cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
+})
