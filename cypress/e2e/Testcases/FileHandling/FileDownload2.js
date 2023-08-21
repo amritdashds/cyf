d79be1e2d65d2798073e5cef7d251b790495f199
Amrit <amritdstest@gmail.com>
2023-08-21 18:47:32 +0530

    EOD

--- cypress/e2e/Testcases/FileHandling/FileDownload2.js
+++ cypress/e2e/Testcases/FileHandling/FileDownload2.js
@@ -13,3 +13,14 @@ describe("File Test", () =>
 
     })
 })
+// const fs = require('fs');
+// const path = require('path');
+// const neatCsv = require('neat-csv');
+
+// const filePath = path.join(__dirname, 'users.csv');
+// fs.readFile(filePath, (error, data) => {
+//   if (error) {
+//     return console.log('error reading file');
+//   }
+//   neatCsv(data)
+//     .then((parsedData) => console.log(parsedData));
--- cypress/e2e/Testcases/FileHandling/FileUpload11.js
+++
@@ -1,8 +0,0 @@
-cy.get("div[class='example'] h3").should('have.text','File Uploaded!');
-cy.get("div[class='example'] h3").should('have.text','File Uploaded!');
-cy.get("div[class='example'] h3").should('have.text','File Uploaded!');
-cy.get("div[class='example'] h3").should('have.text','File Uploaded!');
-cy.get("div[class='example'] h3").should('have.text','File Uploaded!');
-cy.get("div[class='example'] h3").should('have.text','File Uploaded!');
-cy.get("div[class='example'] h3").should('have.text','File Uploaded!');
-cy.get("div[class='example'] h3").should('have.text','File Uploaded!');
