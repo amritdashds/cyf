9d29731a989858e93a12a1ecb06f114b7584e84b
Amrit <amritdstest@gmail.com>
2023-08-21 18:46:49 +0530

    EOD

---
+++ cypress/e2e/Testcases/FileHandling/FileUpload11.js
@@ -0,0 +1,8 @@
+cy.get("div[class='example'] h3").should('have.text','File Uploaded!');
+cy.get("div[class='example'] h3").should('have.text','File Uploaded!');
+cy.get("div[class='example'] h3").should('have.text','File Uploaded!');
+cy.get("div[class='example'] h3").should('have.text','File Uploaded!');
+cy.get("div[class='example'] h3").should('have.text','File Uploaded!');
+cy.get("div[class='example'] h3").should('have.text','File Uploaded!');
+cy.get("div[class='example'] h3").should('have.text','File Uploaded!');
+cy.get("div[class='example'] h3").should('have.text','File Uploaded!');
